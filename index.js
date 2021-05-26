430ad75df9c8d4f3697ecef14d240271d48638a5
steffent1493 <steffent1493@gmail.com>
2021-05-26 12:22:23 -0400

    mission accomplished

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert); 
+// }
+
 function addingEventListener() {
-    const input = document.getElementById('input');
+const input = document.getElementById('input');
 
 function clickAlert() {
   alert('I was clicked!');
 }
 
-input.addEventListener('click', clickAlert); 
+input.addEventListener('click', clickAlert);
 }
